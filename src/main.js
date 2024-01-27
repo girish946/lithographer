75fc30c9add90ef22f69e743c969be8fde388abb
Girish Joshi <mail@girishjoshi.io>
2024-01-27 12:42:09 +0530

    added devices list to the drop down
    Signed-off-by: Girish Joshi <mail@girishjoshi.io>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 const { invoke } = window.__TAURI__.tauri;
 
-
-window.addEventListener("DOMContentLoaded", () => {
+function add_storage_device_names() {
   invoke("get_storage_devices").then((res) => {
     console.log(res);
+    var x = document.getElementById("diskSelect");
+    res.map(myFunction);
+
+    function myFunction(value, index, array) {
+      var option = document.createElement("option");
+      const device_name = JSON.parse(value); // Parse the JSON string to an object
+      console.log(device_name);
+      option.text = device_name.model_name;
+      option.value = device_name.device_name;
+      x.add(option);
+    }
+
   });
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  add_storage_device_names();
 });
